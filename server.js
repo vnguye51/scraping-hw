4ef8e0643e0e15b6927d273192433557a73c9af7
Vincent Nguyen <vincenttsnguyen@gmail.com>
2018-09-27 01:07:42 -0700

    bugfixes

--- server.js
+++ server.js
@@ -108,20 +108,6 @@ app.post('/unsave/:id',function(req,res){
     })
 })
 
-// Route for getting all Articles from the db
-app.get("*", function(req, res) {
-  // Grab every document in the Articles collection
-  db.Article.find({})
-    .then(function(dbArticle) {
-      // If we were able to successfully find Articles, send them back to the client\
-      res.render('index',{dbArticle: dbArticle});
-    })
-    .catch(function(err) {
-      // If an error occurred, send it to the client
-      res.json(err);
-    });
-});
-
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
   // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
@@ -137,6 +123,22 @@ app.get("/articles/:id", function(req, res) {
       res.json(err);
     });
 });
+
+
+// Route for getting all Articles from the db
+app.get("/", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client\
+      res.render('index',{dbArticle: dbArticle});
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
 
 // Route for saving/updating an Article's associated Note
 app.post("/articles/:id", function(req, res) {
